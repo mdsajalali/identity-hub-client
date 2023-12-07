36e17d6ef9938fa43d386249a4cc656a7810ad27
mdsajalali <mdsajalali2020@gmail.com>
2023-12-07 18:47:10 +0600

    loading added

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { FaPenToSquare } from "react-icons/fa6";
 import { MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
@@ -5,8 +6,10 @@ import Swal from "sweetalert2";
 
 const AllUsers = ({ item, idx, user, setUser }) => {
   const { _id, name, email, gender, status } = item;
+  const [isLoading, setIsLoading] = useState(false);
 
   const userDelete = (_id) => {
+    setIsLoading(true);
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -32,31 +35,54 @@ const AllUsers = ({ item, idx, user, setUser }) => {
               const remaining = user.filter((user) => user._id !== _id);
               setUser(remaining);
             }
+          })
+          .finally(() => {
+            setIsLoading(false);
           });
+      } else {
+        setIsLoading(false);
       }
     });
   };
 
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(fetchData);
+  }, []);
+
   return (
     <>
-      <td className="border p-2">{idx + 1}</td>
-      <td className="border capitalize p-2">{name}</td>
-      <td className="border p-2">{email}</td>
-      <td className="border capitalize p-2">{gender}</td>
-      <td className="border capitalize p-2">{status}</td>
-      <td className="border p-2">
-        <Link to={`/updateUser/${_id}`}>
-          <button className="  bg-[#06D6A0] hover:opacity-50 transition-all text-white rounded-sm p-1 md:mr-4 mr-2 ">
-            <FaPenToSquare size={20} />
-          </button>
-        </Link>
-        <button
-          className=" bg-[#EF4C53] hover:opacity-50 transition-all text-white rounded-sm p-1"
-          onClick={() => userDelete(_id)}
-        >
-          <MdDeleteForever size={20} />
-        </button>
-      </td>
+      {isLoading ? (
+        <>
+          <span className="loading loading-spinner text-primary"></span>
+        </>
+      ) : (
+        <>
+          <td className="border p-2">{idx + 1}</td>
+          <td className="border capitalize p-2">{name}</td>
+          <td className="border p-2">{email}</td>
+          <td className="border capitalize p-2">{gender}</td>
+          <td className="border capitalize p-2">{status}</td>
+          <td className="border p-2">
+            <Link to={`/updateUser/${_id}`}>
+              <button className="  bg-[#06D6A0] hover:opacity-50 transition-all text-white rounded-sm p-1 md:mr-4 mr-2 ">
+                <FaPenToSquare size={20} />
+              </button>
+            </Link>
+            <button
+              className=" bg-[#EF4C53] hover:opacity-50 transition-all text-white rounded-sm p-1"
+              onClick={() => userDelete(_id)}
+              disabled={isLoading}
+            >
+              <MdDeleteForever size={20} />
+            </button>
+          </td>
+        </>
+      )}
     </>
   );
 };
